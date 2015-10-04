8a46a0ce2823ef5a7f7015bff5ea662df391f260
Atul Bhosale <atul1bhosale@gmail.com>
2015-10-04 10:50:16 +0530

    Added Active Admin structure

---
+++ app/admin/user.rb
@@ -0,0 +1,19 @@
+ActiveAdmin.register User do
+
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if resource.something?
+#   permitted
+# end
+  member_action :lock, method: :put do
+    resource.lock!
+    redirect_to resource_path, notice: "Locked!"
+  end
+end
